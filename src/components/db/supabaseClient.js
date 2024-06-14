35cd45dcc64684558783d3b84497da0c989852ee
Maltstar <hftool@proton.me>
2024-06-14 18:34:09 +0200

    test vercel

--- src/components/db/supabaseClient.js
+++ src/components/db/supabaseClient.js
@@ -2,8 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 
 //const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 //const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_KEY
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_URL
+
+const supabaseUrl = 'https://xlyocekrqfonxwtbeyys.supabase.co/'
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhseW9jZWtycWZvbnh3dGJleXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc3OTY5MTEsImV4cCI6MjAzMzM3MjkxMX0.Jn94kNIne8Hla8AuczHi8ORV37XofnXcJgNJps6TqZA'
+
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase
